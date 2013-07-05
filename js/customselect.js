16973bad3ee2e89a345e38d0b8a85908a1456046
Kailash <kailash.p@openjuice.in>
2013-07-05 20:28:14 +0530

    CSS changes
    Signed-off-by: Kailash <kailash.p@openjuice.in>

--- js/customselect.js
+++ js/customselect.js
@@ -154,4 +154,4 @@
     });
 })(jQuery);
 
-$('.header_right .first_row select, .tab_content_left select').customSelect();
+$('.header_right .first_row select, .tab_content_left select, .inner_block div select.styled').customSelect();

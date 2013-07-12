f8ab7a134b48402e2168117f4cafa23d28b800f9
Openly CI <abhi.h@openly.co>
2013-07-12 15:36:00 +0530

    jenkins-Richemont Build-27

---
+++ build/js/main-jobs.js
@@ -0,0 +1,1 @@
+require.config({baseUrl:"js",paths:{jquery:"vendor/jquery-1.9.1.min",modernizr:"vendor/modernizr-2.6.2.min"}}),require(["jquery","modernizr","navigation","search","customselect","richemont-main"]);
---
+++ build/js/search.js
@@ -0,0 +1,1 @@
+$(".search").mouseover(function(){$(".search_block").show(),$(".search_block").find("input:text").focus()});var hideTimer;$(".search_block").find("input:text").blur(function(){$(".search_block").hide()});

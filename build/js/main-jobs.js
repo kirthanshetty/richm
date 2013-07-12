9ebd7d10ad62d107ea95106c90e963275b92595d
Openly CI <abhi.h@openly.co>
2013-07-12 18:18:57 +0530

    jenkins-Richemont Build-28

--- build/js/main-jobs.js
+++ build/js/main-jobs.js
@@ -1,1 +1,1 @@
-require.config({baseUrl:"js",paths:{jquery:"vendor/jquery-1.9.1.min",modernizr:"vendor/modernizr-2.6.2.min"}}),require(["jquery","modernizr","navigation","search","customselect","richemont-main"]);
+require.config({baseUrl:"js",paths:{jquery:"vendor/jquery-1.9.1.min"}}),require(["jquery","navigation","search","customselect","richemont-main"]);

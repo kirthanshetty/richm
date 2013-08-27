13be6dcc50d1e25b429b08ee91053a100d277aad
Openly CI <abhi.h@openly.co>
2013-08-27 16:35:52 +0530

    jenkins-Richemont Build-127

---
+++ build/js/find_browser.js
@@ -0,0 +1,1 @@
+navigator.userAgent.indexOf("Safari")!=-1&&navigator.userAgent.indexOf("Chrome")==-1&&($(".maisons_lists ul li").removeClass("fading").addClass("shading"),$(".maison_close").click(function(){setTimeout(function(){$(".maisons_lists ul li").removeClass("shading")},1e3)}));

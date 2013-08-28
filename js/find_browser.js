81c96ae98c8b1051317f9b62681e879613cefa18
Kailash <kailash.p@openjuice.in>
2013-08-28 13:39:32 +0530

    Our Maisons issue fixes
    Signed-off-by: Kailash <kailash.p@openjuice.in>

--- js/find_browser.js
+++ js/find_browser.js
@@ -1,9 +1,4 @@
 if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) 
 	{
 		$('.maisons_lists ul li').removeClass('fading').addClass('shading');
-		$('.maison_close').click(function(){
-			setTimeout(function() {
-            	$('.maisons_lists ul li').removeClass('shading');
-        	}, 1000);
-        });
     };

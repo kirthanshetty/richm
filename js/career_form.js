d0564e3e57fe03e4cd870e8d6db80199ed375130
Kailash <kailash.p@openjuice.in>
2013-08-30 19:13:35 +0530

    Career form fixes
    Signed-off-by: Kailash <kailash.p@openjuice.in>

--- js/career_form.js
+++ js/career_form.js
@@ -1,12 +1,10 @@
-$(function() {
- var container = $(".form_submission");
-	container.delegate('.submission a', 'click' ,function(e){
-		e.preventDefault();
-		var link = this.href,				
-			anchor = $(this),
-			container = anchor.parent().parent().parent();
-		$.get(link,function(data){
-			container.html(data);
-		});
+
+$(function(){
+	$('.form_submission a.prev').click(function(){
+		$(this).parent().parent().hide().prev().show();
+	});
+	$('.form_submission a.next').click(function(){
+		$(this).parent().parent().hide().next().show();
 	});
 });
+

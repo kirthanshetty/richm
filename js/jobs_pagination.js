70a86cbfeefb80c0b5d8c03b2fec37572f7077da
Abhilash  Hebbar <abhi@openjuice.in>
2013-09-02 18:01:25 +0530

    JS validation etc.

--- js/jobs_pagination.js
+++ js/jobs_pagination.js
@@ -10,11 +10,22 @@ $(function() {
 		});
 	});
 	$('#job_alert').click(function(){
-		$('.job_dialog').show();
+		$('.job_dialog, .job_dialog .activate').show();
+		var content = $('.job_dialog .activate .content:first').show();
+		content.find('form').submit(function(e){
+			e.preventDefault();
+			var emailVal = $(this).find('#email').val();
+			if(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/.test(emailVal.toUpperCase())){
+				content.hide().next().show();
+			}else{
+				$(this).find('#email').parent().addClass('error');
+			}
+			return false;
+		})
 		$('body').append('<div class="overlay"></div>');
 	});
 	$('#close_alert').click(function(){
-		$('.job_dialog').hide();
+		$('.job_dialog, .job_dialog .section_left').hide();
 		$('.overlay').remove();
 	})
 });

68a0dd7040a0b292c53a562dcc9c11112eda0876
Kailash <kailash.p@openjuice.in>
2013-10-01 17:06:09 +0530

    Custom Select Fields Broken After Carousel Navigation
    Signed-off-by: Kailash <kailash.p@openjuice.in>

--- js/career_form.js
+++ js/career_form.js
@@ -1,17 +1,20 @@
+
 // -----------------------------------------------------------------
 //                        REQUIREJS MODULE
 require(['jquery'],function(){
 // -----------------------------------------------------------------
-$(function(){
-	$('.form_submission a.prev').click(function(){
-		$(this).parent().parent().hide().prev().show();
-	});
-	$('.form_submission a.next').click(function(){
-		$(this).parent().parent().hide().next().show();
-	});
-});
+	 	$('.tab_content_full').delegate('.form_submission a.prev', 'click', function(e){
+	 		e.preventDefault();
+			$(this).parent().parent().hide().prev().show();
+		});
 
+		$('.tab_content_full').delegate('.form_submission a.next', 'click', function(e){
+			e.preventDefault();
+			$(this).parent().parent().hide().next().show();
+		});
+	});
+// -----------------------------------------------------------------
 
 // -----------------------------------------------------------------
-})
-// -----------------------------------------------------------------
+
+

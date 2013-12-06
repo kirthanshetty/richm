43478ac904fd543c0be492b1909eaf8d6b0aa5c3
kailash <kailash.p@openly.co>
2013-12-06 17:39:53 +0530

    Fix for [issue #506]

--- js/find_browser.js
+++ js/find_browser.js
@@ -4,8 +4,8 @@ require(['jquery'],function(){
 // -----------------------------------------------------------------
 if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) 
 	{
-		$('.maisons_lists ul li').removeClass('fading').addClass('shading');
-		$('.page_1 .row .col.span_4:last-child img').css({'position':'relative','right': '-0.5%'});
+		// $('.maisons_lists ul li').removeClass('fading').addClass('shading');
+		// $('.page_1 .row .col.span_4:last-child img').css({'position':'relative','right': '-0.5%'});
     };
     
 // -----------------------------------------------------------------

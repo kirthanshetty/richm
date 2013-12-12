2ee6b092eb3177c8afa976d9841d7378745e14df
kailash <kailash.p@openly.co>
2013-12-12 11:31:26 +0530

    safari fix

--- js/find_browser.js
+++ js/find_browser.js
@@ -6,6 +6,7 @@ if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('
 	{
 		// $('.maisons_lists ul li').removeClass('fading').addClass('shading');
 		// $('.page_1 .row .col.span_4:last-child img').css({'position':'relative','right': '-0.5%'});
+		$('.page_1 .row .col.span_4:last-child').css({'position':'relative','right':'-0.183%'});
     };
     
 // -----------------------------------------------------------------
